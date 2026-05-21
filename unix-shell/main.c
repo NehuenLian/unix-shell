aff60233b3fcaf5a3d2aa7466caa5bd32684ed42
Nehuen Lian <nehuenn.lian@gmail.com>
2026-05-21 02:25:21 -0300

    add functions
    lsh_read_line
    
    lsh_loop

--- unix-shell/main.c
+++ unix-shell/main.c
@@ -1,8 +1,70 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#define LSH_RL_BUFSIZE 1024
+
+char* lsh_read_line(void)
 {
-	printf("Hello World.\n");
-	return 0;
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char* buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if (!buffer) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		// Read a character
+		c = getchar();
+
+		// If we hit eof, replace it with a null character and return.
+		if (c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		}
+		else {
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= bufsize) {
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer) {
+				fprintf(stderr, "lsh: reallocation error.\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+
+void  lsh_loop(void)
+{
+	char* line;
+	char** args;
+	int status;
+
+	do {
+		printf("> ");
+		line = read_lsh_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+
+int main(int argc, char** argv)
+{
+	// Load config files
+
+	// Run command loop
+	lsh_loop();
+
+	// Perform any shutdown/cleanup
+	return EXIT_SUCCESS;
 }
