ec591a6403bdd9adf793acd7719d7a4a1403454b
Nehuen Lian <nehuenn.lian@gmail.com>
2026-05-26 05:29:45 -0300

    add forward declarations
    lsh_split_line lsh_execute lsh_launch

--- unix-shell/main.c
+++ unix-shell/main.c
@@ -14,6 +14,10 @@ int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
+char **lsh_split_line(char *line);
+int lsh_execute(char **args);
+int lsh_launch(char **args);
+
 char *lsh_read_line(void)
 {
 	int bufsize = LSH_RL_BUFSIZE;
