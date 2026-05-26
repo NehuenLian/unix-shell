c1e5ee82ad839dad9f25d58a131b981bf020fac5
Nehuen Lian <nehuenn.lian@gmail.com>
2026-05-26 00:26:49 -0300

    add forward declarations
    lsh_cd lsh_help lsh_exit

--- unix-shell/main.c
+++ unix-shell/main.c
@@ -10,6 +10,10 @@
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
 char* lsh_read_line(void)
 {
 	int bufsize = LSH_RL_BUFSIZE;
